fc5110c508afb2c97545c8fff42252c115d3c317
przemyslawka <hexoren@gmail.com>
2023-07-03 20:00:38 +0200

    dodanie serwera

---
+++ main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+    region = "us-east-2"
+}
+resource "aws_instance" "example" {
+    ami = "ami-0fb653ca2d3203ac1"
+    instance_type   =   "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "witaj, świecie" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+    user_data_replace_on_change = true
+
+    tags = {
+        Name = "terraform-example"
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = var.server_port
+        to_port = var.server_port
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+variable "server_port" {
+    description = "Port number used by the server for HTTP "
+    type = number
+    default = 8080 
+}
+
+output "public_ip" {
+    value = aws_instance.example.public_ip
+    description = "Public IP address of the server"
+  
+}
